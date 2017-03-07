442516f6762950577f40a2995834de979d0b0058
Michael Mitchell <mmitchell10000@gmail.com>
2017-03-07 17:28:33 -0600

    Networking Project
    Networking project-
    
    Lobby-
    Player Color Choice
    
    Name the player chooses, becomes their Gameobject name (for easy access
    to possible player name on HUD)
    
    Main Scene-
    Player Movement
    Player Rotation w/ mouse
    
    Known issue-
    
    When testing on the same computer, opening two different builds,
    rotation occurs for each player object, possibly due to the build being
    on the same PC. This might be a simple fixing when testing of seperate
    builds on seperate PC's. If any questions, dont hesitate to text!

---
+++ Networking/Assets/Scripts/Network/NetworkLobbyHook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prototype.NetworkLobby;
+using UnityEngine.Networking;
+
+//Will override the current LobbyHook
+public class NetworkLobbyHook : LobbyHook {
+
+    //When player loads, takes lobbyPlayer info, gamePlayer, and giving lobby player levels
+    //to the gameplayer
+
+    //This connects all values that are in player info to values that are in the game
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        //Grab lobby player from PlayerInfo
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        //Grab local player
+        SetupLocalPlayer localPlayer = gamePlayer.GetComponent<SetupLocalPlayer>();
+
+        localPlayer.playerColor = lobby.playerColor;
+        localPlayer.pname = lobby.playerName;
+    }
+
+}
---
+++ Networking/Assets/Scripts/Network/SetupLocalPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    //Sends info through the server
+    [SyncVar]
+    public string pname = "player";
+    //Sends info through the server
+    [SyncVar]
+    public Color playerColor = Color.white;
+
+	// Use this for initialization
+	void Start () {
+
+        //If local player, enable
+        if (isLocalPlayer)
+        {
+            GetComponent<Player>().enabled = true;
+            GetComponent<PlayerStateMachine>().enabled = true;
+
+        }
+
+        //Grabs hold of all of the renderers
+        Renderer[] rends = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rends)
+            r.material.color = playerColor;
+
+        //Change spawn of player
+        this.transform.position = new Vector3(Random.Range(-17, 17), 0, Random.Range(-17, 17));
+
+        this.transform.name = pname;
+	}
+
+}
